0876c4b5696ee2cf0c57d744f1dd9b6b6d393861
Nisida Azzalini <nazzalini@Nisidas-MacBook-Pro.local>
2020-09-02 18:26:54 +1200

    add a Friend class and createFriend resolver

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ app.get('/', (req, res) => {
 	res.send('GraphQL is amazing!');
 });
 
+//use this class to initialize a new friend in our database
+class Friend {
+	constructor(id, { firstName, lastName, gender, language, email }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.language = language;
+		this.email = email;
+	}
+}
+
+//initialize our database as an empty object
+const friendDatabase = {};
+
 //resolver for GraphQL
 //it needs to match the type in schema
 const root = {
@@ -18,8 +33,14 @@ const root = {
 			lastName: 'Flower',
 			gender: 'female',
 			language: 'english',
-			emails: [{ email: 'daisy@flower.com' }, { email: 'd_flower@live.com' }],
+			email: 'daisy@flower.com',
 		};
+	},
+	createFriend: ({ input }) => {
+		let id = require('crypto').randomBytes(10).toString('hex');
+		//assign a position in the DB
+		friendDatabase[id] = input;
+		return new Friend(id, input);
 	},
 };
 
