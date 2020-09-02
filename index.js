96531fa8fcf18e4dbc2107c9e853d45423354af6
Nisida Azzalini <nazzalini@Nisidas-MacBook-Pro.local>
2020-09-02 17:12:56 +1200

    install graphql dependencies

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('graphql', graphqlHTTP({
     schema:schema,
     rootValue: root,
     graphiql: true
-}) )
+}));
 
 app.listen(8080, () => 
     console.log('Running server on port localhost:8080/graphql')
