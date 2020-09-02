7a4d16216c15483c642c3dd04ea5adfc2f66e63e
Nisida Azzalini <nazzalini@Nisidas-MacBook-Pro.local>
2020-09-03 08:33:18 +1200

    create a resolvers file and copy resolvers and class from index

---
+++ resolvers.js
@@ -0,0 +1,30 @@
+//use this class to initialize a new friend in our database
+class Friend {
+	constructor(id, { firstName, lastName, gender, language, email }) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.language = language;
+		this.email = email;
+	}
+}
+
+//initialize our database as an empty object
+const friendDatabase = {};
+
+//resolver for GraphQL
+//it needs to match the type in schema
+const resolvers = {
+	getFriend: ({ id }) => {
+		return new Friend(id, friendDatabase[id]);
+	},
+	createFriend: ({ input }) => {
+		let id = require('crypto').randomBytes(10).toString('hex');
+		//assign a position in the DB
+		friendDatabase[id] = input;
+		return new Friend(id, input);
+	},
+};
+
+export default resolvers;
