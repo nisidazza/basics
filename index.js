4fad8e65a85a6bc260983e8eebfbb00ba9512dfe
Nisida Azzalini <nazzalini@Nisidas-MacBook-Pro.local>
2020-09-02 17:45:42 +1200

    install prettier

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 import express from 'express';
-import {graphqlHTTP} from 'express-graphql';
+import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
-
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('GraphQL is amazing!')
-})
+app.get('/', (req, res) => {
+	res.send('GraphQL is amazing!');
+});
 
 //resolver for GraphQL
-const root = {hello: () => "Hi, I am Nisida"};
+//it needs to match the type in schema
+const root = {
+	friend: () => {
+		return {
+			id: 28718992,
+			firstName: 'Daisy',
+			lastName: 'Flower',
+			gender: 'female',
+			language: 'english',
+			email: 'daisy@flower.com',
+		};
+	},
+};
 
 //pass the query through the schema
 //use graphiql to test what we do in the GraphQL server
-app.use('/graphql', graphqlHTTP({
-    schema:schema,
-    rootValue: root,
-    graphiql: true
-}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
-app.listen(8080, () => 
-    console.log('Running server on port localhost:8080/graphql')
-)
+app.listen(8080, () =>
+	console.log('Running server on port localhost:8080/graphql')
+);
