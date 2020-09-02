906bf4aa61d4356ae89a71e83817324a3c6bf185
Nisida Azzalini <nazzalini@Nisidas-MacBook-Pro.local>
2020-09-02 17:01:21 +1200

    update index.js in order to be able to run GraphQL

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from './schema';
+
 
 const app = express();
 
 app.get('/', (req,res) => {
     res.send('GraphQL is amazing!');
 })
+
+//resolver for GraphQL
+const root = {hello: () => "Hi, I am Nisida"};
+
+//pass the query through the schema
+//use graphiql to test what we do in the GraphQL server
+app.use('graphql', graphqlHTTP({
+    schema:schema,
+    rootValue: root,
+    graphiql: true
+}) )
 
 app.listen(8080, () => 
     console.log('Running server on port localhost:8080/graphql')
